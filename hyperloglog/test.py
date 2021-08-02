c6e2ad854f12d458c382f28e4bc81a0f971a9847
Huy Nguyen <huynguyen@Huys-MacBook-Air.local>
2021-08-02 14:03:52 -0700

    Add boundary test for n cardinality

--- hyperloglog/test.py
+++ hyperloglog/test.py
@@ -14,7 +14,7 @@ def standard_error(m):  # Threshold for testing.
 
 '''
 
-    *We need at max log2 (n) bits to estimate n cardinality!
+    *We need at max log2(n) bits to estimate n cardinality!
     This test is written based on this assumption.
 
 '''
@@ -52,7 +52,7 @@ class HLLTest(unittest.TestCase):
         assert(error_rate <= accepted_error_rate)
 
     def test_3(self):
-        size = int(1e8)
+        size = int(1e5)
         data = np.random.randint(low=(-1)*int(1e10), high=int(1e10), size=size)
         b = int(np.log2(size))  # Num bits needed.
         hll = HyperLogLog(hash_fn=hashlib.sha1, num_bits=b)
@@ -61,6 +61,22 @@ class HLLTest(unittest.TestCase):
             hll.add(val)
             my_set.add(val)
         my_set_n = len(my_set)
+        hll_n = hll.get_num_distinct()
+        error_rate = np.abs(float(my_set_n-hll_n)/float(my_set_n+hll_n))
+        accepted_error_rate = np.abs(standard_error(2**b))
+        assert(error_rate <= accepted_error_rate)
+
+    def test_4(self):  # Boundary n test
+        size = int(1e6)
+        data = np.random.randint(low=(-1)*int(1e10), high=int(1e10), size=size)
+        my_set = set()
+        for val in data:
+            my_set.add(val)
+        my_set_n = len(my_set)
+        b = int(np.log2(my_set_n))  # Num bits needed.
+        hll = HyperLogLog(hash_fn=hashlib.sha1, num_bits=b)
+        for val in data:
+            hll.add(val)
         hll_n = hll.get_num_distinct()
         error_rate = np.abs(float(my_set_n-hll_n)/float(my_set_n+hll_n))
         accepted_error_rate = np.abs(standard_error(2**b))
